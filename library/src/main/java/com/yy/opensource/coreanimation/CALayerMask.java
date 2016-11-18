61bb75a4d53f23e8c6b34d5711e88de644fada0f
PonyCui <cuis@vip.qq.com>
2016-11-18 11:31:09 +0800

    try to fix mask issue.

--- library/src/main/java/com/yy/opensource/coreanimation/CALayerMask.java
+++ library/src/main/java/com/yy/opensource/coreanimation/CALayerMask.java
@@ -25,33 +25,30 @@ class CALayerMask {
         gl.glClearStencil(0);
         gl.glColorMask(false, false, false, false);
         gl.glDepthMask(false);
-        FloatBuffer lastBuffer = null;
         CALayer current = layer;
+        CALayer target = null;
         gl.glColorPointer(4, GL10.GL_FLOAT, 0, CGColor.whiteColor.colorBuffer);
         while (current != null) {
             if (current.masksToBounds) {
-                enabled = true;
-                if (lastBuffer != null) {
-                    gl.glStencilOp(GL10.GL_ZERO, GL10.GL_ZERO, GL10.GL_ZERO);
-                    gl.glVertexPointer(3, GL10.GL_FLOAT, 0, lastBuffer);
-                    gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length / 3);
-                }
-                FloatBuffer vertexBuffer = CALayerHelper.requestVertexBuffer(CALayerHelper.combineTransform(current), current.anchorPoint, current.frame, current.windowBounds);
-                gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
-                gl.glStencilFunc(GL10.GL_ALWAYS, 1, 1);
-                gl.glStencilOp(GL10.GL_REPLACE, GL10.GL_REPLACE, GL10.GL_REPLACE);
-                if (current.mask != null) {
-                    current.mask.surfaceView = current.surfaceView;
-                    current.mask.windowBounds = current.windowBounds;
-                    current.mask.transform = CALayerHelper.combineTransform(current);
-                    current.mask.drawContents(gl);
-                }
-                else {
-                    gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length / 3);
-                }
-                lastBuffer = vertexBuffer;
+                target = current;
             }
             current = current.superLayer;
+        }
+        if (target != null) {
+            enabled = true;
+            FloatBuffer vertexBuffer = CALayerHelper.requestVertexBuffer(CALayerHelper.combineTransform(target), target.anchorPoint, target.frame, target.windowBounds);
+            gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+            gl.glStencilFunc(GL10.GL_ALWAYS, 1, 1);
+            gl.glStencilOp(GL10.GL_REPLACE, GL10.GL_REPLACE, GL10.GL_REPLACE);
+            if (target.mask != null) {
+                target.mask.surfaceView = target.surfaceView;
+                target.mask.windowBounds = target.windowBounds;
+                target.mask.transform = CALayerHelper.combineTransform(target);
+                target.mask.drawContents(gl);
+            }
+            else {
+                gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length / 3);
+            }
         }
         gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
         gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
